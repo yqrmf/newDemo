77cd0a62910ffde37870d2296636743cda78b6eb
yqrmf <2646208003@qq.com>
2024-06-21 18:00:50 +0800

    Add files via upload

---
+++ newDemo/src/main/java/com/example/newdemo/demos/web/service/WarnDetailDTO.java
@@ -0,0 +1,51 @@
+package com.example.newdemo.demos.web.service;
+
+public class WarnDetailDTO {
+    private Integer carId;
+    private String batteryType;
+    private String warnName;
+    private Integer warnLevel;
+
+    public void setCarId(Integer carId) {
+        this.carId = carId;
+    }
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public void setBatteryType(String batteryType) {
+        this.batteryType = batteryType;
+    }
+
+    public String getBatteryType() {
+        return batteryType;
+    }
+
+
+    public void setWarnName(String warnName) {
+        this.warnName = warnName;
+    }
+
+    public String getWarnName() {
+        return warnName;
+    }
+
+    public void setWarnLevel(Integer warnLevel) {
+        this.warnLevel = warnLevel;
+    }
+
+    public Integer getWarnLevel() {
+        return warnLevel;
+    }
+
+    @Override
+    public String toString() {
+        return  "{" + '\n' +
+                "车架编号：" + carId + '\n' +
+                "电池类型：\"" + batteryType + '\"' + '\n' +
+                "warnName：'" + warnName + '\'' + '\n' +
+                "warnLevel：" + warnLevel + '\n' +
+                '}';
+    }
+}
---
+++ newDemo/src/test/java/com/example/newdemo/NewDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.newdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
