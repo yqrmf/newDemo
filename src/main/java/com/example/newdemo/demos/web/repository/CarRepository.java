2875e8318b897bd0376a066bf3fb8bab785c5fc7
yqrmf <2646208003@qq.com>
2024-06-21 18:55:50 +0800

    Delete src directory

--- src/main/java/com/example/newdemo/demos/web/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.newdemo.demos.web.repository;
-
-
-import com.example.newdemo.demos.web.bean.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-    Car findByCarId(Integer carId);
-}
